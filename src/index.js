9fc92a91baa66fde648faef3ceac4f117a1fffe7
DennyNguyen123 <101079975+DennyNguyen123@users.noreply.github.com>
2022-05-11 15:17:02 +0700

    make it better

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const liveReload = require("livereload")
 const connectLivereload = require("connect-livereload")
 
 const publicDirection = path.join(__dirname,'public');
+const port = 3000
+
+// Live reload page when change code
 liveReloadServer = liveReload.createServer()
 liveReloadServer.watch(publicDirection)
 liveReloadServer.server.once("connection", () => {
@@ -13,33 +16,49 @@ liveReloadServer.server.once("connection", () => {
     liveReloadServer.refresh("/");
   }, 100);
 });
+
 const app = express()
-app.use(connectLivereload());
-const port = 3000
-
 app.use(express.static(publicDirection))
+app.use(connectLivereload());
 
-//Http logger
-app.use(morgan("combined"))
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json())
+// // Http logger
+// app.use(morgan("combined"))
+
 
 //Template engine
 app.engine('hbs', engine({
   extname:".hbs"
 }));
-
 app.set('view engine', 'hbs');
 // app.set('views', './views');
 app.set('views', path.join(__dirname,'resources\\views'));
 
+
+
 app.get('/', (req, res) => {
+  // console.log("Log")
   res.render('home')
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news')
-})
+});
+
+app.get('/search', (req, res) => {
+  res.render('search')
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.params.q)
+  console.log(req.body)
+  res.send('1')
+});
 
 
 app.listen(port, () => {
-  console.log(`Port http://localhost:${port}`)
+  console.log(`http://localhost:${port}`)
 })
